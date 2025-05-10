cbdf44180d78143dc5c594af0cd5bf3bed1edf4d
diazrenan <rdrenandiaz@gmail.com>
2025-05-10 16:10:40 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,6 @@
+document.getElementById("meuBotao").addEventListener("click", function () {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    });
+});
